76789ba413a6d0225f50f4162a44945c77764f28
khaledkaram510 <khaleddaib510@gmail.com>
2024-08-27 21:09:18 +0300

    chore: Remove password length validation in signup

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const {connectDB} = require("./database/connection");
 // const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const multerConfig = require("./middleware/multerConfig");
+const authRoutes = require("./routes/authRoutes");
 const cors = require("cors");
+
 const app = express();
 
 const PORT = 5000;
@@ -11,23 +14,15 @@ const PORT = 5000;
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
-mongoose
-  .connect(
-    "mongodb+srv://amrateia46:Complex2024@gym-nutrition-plan.rsmkz.mongodb.net/?retryWrites=true&w=majority&appName=Gym-Nutrition-Plan"
-  )
-  .then((result) => {
-    console.log("Connected to the database");
-    app.listen(PORT, (req, res, next) => {
-      console.log(`Server running on port ${PORT}`);
-      //const userRoutes = require("./routes/userRoutes");
-      const authRoutes = require("./routes/authRoutes");
 
-      //app.use("/user", userRoutes);
-      app.use("/auth", authRoutes);
+//app.use("/user", userRoutes);
+app.use("/auth", authRoutes);
+app.get("/", (req, res) =>{
+  res.send("Hello World!");
+});
 
-      app.get("/", function (req, res) {
-        res.send("Hello World!");
-      });
-    });
-  })
-  .catch((err) => console.log(err));
+connectDB;
+
+app.listen(PORT, (req, res, next) => {
+  console.log(`Server running on http://localhost:${PORT}/`);
+});
--- server/middleware/signupAuthenticate.js
+++ server/middleware/signupAuthenticate.js
@@ -5,12 +5,15 @@ const signupAuthenticate = [
   isLength({min: 3}).
   withMessage('Username must be at least 3 characters long'),
   body('email').
+  trim().
   isEmail().
   withMessage('Please enter a valid email address'),
   body('password').
+  trim().
   isLength({min: 8}).
   withMessage('Password must be at least 8 characters long'),
   body('gender').
+  trim().
   notEmpty().
   withMessage('Please select gender')
 ];
