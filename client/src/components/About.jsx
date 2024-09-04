ded55f963bcb1c350d551f115e9eaa12cf32caed
Ahmed Gomaa <ag8496001@gmail.com>
2024-09-04 19:54:53 +0300

    media query

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -8,19 +8,16 @@ const About = () => {
             <div>
                 <p className="text-3xl font-medium text-center p-3 transition-all text-green-950  hover:shadow-[#007654] hover:shadow-lg hover:rounded-2xl">Welcome to GYMNUT, your trusted partner in achieving a healthier lifestyle through balanced nutrition and personalized meal planning. We believe that good food is the foundation of a happy, healthy life, and we're here to guide you on your journey to wellness.</p>
             </div>
-            <div className="flex gap-12 items-center ">
-
+            <div className="flex gap-12 items-center md:flex-row-reverse flex-col ">
+                <img src={about1} className="max-h-80 shadow-lg shadow-[#007654] rounded-3xl max-w-[20rem]" />
                 <div className="space-y-4">
                     <h2 className="text-3xl font-bold">Our Mission</h2>
                     <p className=" text-green-950 text-2xl font-medium p-3 transition-all  hover:shadow-[#007654] hover:shadow-lg hover:rounded-2xl">At GYMNUT, our mission is to make healthy eating accessible, enjoyable, and sustainable. We understand that everyone's nutritional needs and preferences are unique, which is why we offer personalized plans that cater to your specific goals, whether it's weight management or simply adopting a healthier lifestyle.</p>
                 </div>
-
-                <img src={about1} className="max-h-80 transition  shadow-lg shadow-[#007654] rounded-3xl"/>
-
             </div>
 
-            <div className="flex gap-12 items-center ">
-                <img src={about2} className="max-h-80 transition  shadow-lg shadow-[#007654] rounded-3xl"/>
+            <div className="flex gap-12 items-center flex-col md:flex-row  ">
+                <img src={about2} className="max-h-80 shadow-lg shadow-[#007654] rounded-3xl max-w-[20rem]" />
                 <div className="space-y-4">
                     <h2 className="text-3xl font-bold">Why Choose Us?</h2>
                     <p className=" text-green-950 text-2xl font-medium p-3 transition-all  hover:shadow-[#007654] hover:shadow-lg hover:rounded-2xl">We are committed to providing you with the tools, resources, and knowledge to make informed decisions about your diet and nutrition. Our plans are not just about food; they're about building lasting habits that contribute to your overall well-being. With GYMNUT, you can take control of your health with confidence and ease.</p>
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,17 +1,33 @@
 import { Link } from "react-router-dom"
-
+import { GiHamburgerMenu } from "react-icons/gi";
+import { useState } from "react";
 
 const Header = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const toggleNavbar = () => {
+        setIsOpen(!isOpen);
+    }
     return (
-        <header className="px-14 flex py-4 items-center justify-between text-[#fefefe] bg-[#9fb429] min-[320px]:space-x-16">
+        <header className="px-14 sticky top-0 z-[20] mx-auto flex-wrap w-full flex py-4 items-center justify-between text-[#fefefe] bg-[#9fb429]">
             <Link to={"/"} className="text-3xl font-bold">GYMNUT</Link>
-            <nav className="flex items-center justify-center gap-16 text-1xl">
-                <Link to={"/"} className="font-semibold link">HOME</Link>
-                <a className="font-semibold link" href="#About">ABOUT</a>
-                <a className="font-semibold link" href="#Services">SERVICES</a>
-                <a className="font-semibold link" href="#Contact">CONTACT</a>
-                <Link className="font-semibold link" to={"/login"}>LOGIN</Link>
+            <nav className="w-1/3 justify-end flex">
+                <div className="hidden md:flex justify-between w-full">
+                    <Link to={"/"} className="font-semibold link">HOME</Link>
+                    <a className="font-semibold link" href="#About">ABOUT</a>
+                    <a className="font-semibold link" href="#Services">SERVICES</a>
+                    <a className="font-semibold link" href="#Contact">CONTACT</a>
+                    <Link className="font-semibold link" to={"/login"}>LOGIN</Link>
+                </div>
+                <div>
+                    <GiHamburgerMenu className="text-3xl cursor-pointer md:hidden" onClick={toggleNavbar}
+                    />
+                </div>
             </nav>
+            {isOpen && (
+                <div className="flex flex-col basis-full items-center md:hidden">
+                    <NavLinks />
+                </div>
+            )}
         </header>
     )
 }
