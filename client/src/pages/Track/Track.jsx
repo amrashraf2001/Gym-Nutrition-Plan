050d79c67bb2199cadb5c6f7aaaa2feb02d362d2
Ahmed Gomaa <ag8496001@gmail.com>
2024-09-16 17:06:58 +0300

    searchBar

--- client/src/pages/Track/Track.jsx
+++ client/src/pages/Track/Track.jsx
@@ -1,35 +1,50 @@
 import { CiSearch } from "react-icons/ci";
 import { UserContext } from "../../contexts/User"
-import { useContext } from "react";
-import axios from '../../api/axios'
+import { useContext, useEffect, useState } from "react";
 
 const Track = () => {
-
     const loggedData = useContext(UserContext);
     const page = 1;
-    const limit = 3;
+    const limit = 6;
+    const [foodItems, setFoodItems] = useState([])
+
     const searchFood = async (e) => {
         try {
-            console.log(json.parse(loggedData.loggedUser))
-            const response = await axios.get(`/user/foods/${e.target.value}`, {
-                params: {
-                    page: page,
-                    limit: limit
-                },
-                headers: {
-                    'Authorization': `Bearer ${loggedData.loggedUser.token}`
-                }
-            })
-            let food = response.food
+            if (e.target.value === '') {
+                setFoodItems([]);
+            } else {
+                fetch(`http://localhost:5000/user/foods/${e.target.value}?page=${page}&limit=${limit}`, {
+                    method: 'GET',
+                    headers: {
+                        "Authorization": `Bearer ${JSON.parse(loggedData.loggedUser)}`
+                    }
+                })
+                    .then((res) => res.json())
+                    .then(data => {
+                        setFoodItems(data.food);
+                    })
+                    .catch(err => console.log(err))
+            }
         } catch (err) {
             console.log(err);
         }
     }
     return (
         <section className="px-14 flex items-start justify-center py-8 ">
-            <div className="relative w-full">
-                <input type="search" onChange={searchFood} className=" p-1 rounded-lg w-full h-16 pl-8 outline-none" id="search" placeholder="Search" />
-                <label htmlFor="search" className="absolute text-black text-2xl top-1/3 left-1 z-30"><CiSearch /></label>
+            <div className="w-full">
+                <div className="relative w-full">
+                    <input type="search" onChange={searchFood} autoComplete="off" className=" p-1 rounded-lg w-full h-16 pl-8 outline-none" id="search" placeholder="Search" />
+                    <label htmlFor="search" className="absolute text-black text-2xl top-1/3 left-1 z-30"><CiSearch /></label>
+                </div>
+                <div className="w-full px-10 py-5 bg-gray-400 ">
+                    {
+                        foodItems.map((item) => {
+                            return (
+                                <p className="font-semibold text-2xl" key={item._id}>{item.name}</p>
+                            )
+                        })
+                    }
+                </div>
             </div>
         </section>
     )
