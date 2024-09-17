bcad458bed30a6d5f0f6aa1d7dafaba3244c7688
Ahmed Gomaa <ag8496001@gmail.com>
2024-09-18 00:37:36 +0300

    "Updated Track.jsx: modified CSS classes and added/removed elements in the component"

--- client/src/pages/Track/Track.jsx
+++ client/src/pages/Track/Track.jsx
@@ -37,15 +37,15 @@ const Track = () => {
     }
 
     return (
-        <section className="px-14 flex flex-col items-start justify-center py-8 ">
-            <div className="w-full">
+        <section className="px-14 flex flex-col items-start justify-center py-4 ">
+            <div className="w-full relative">
                 <div className="relative w-full">
                     <input type="search" onChange={searchFood} autoComplete="off" className=" p-1 rounded-lg w-full h-16 pl-8 outline-none" id="search" placeholder="Search" />
                     <label htmlFor="search" className="absolute text-black text-2xl top-1/3 left-1 z-30"><CiSearch /></label>
                 </div>
                 {
                     foodItems.length !== 0 ?
-                        <div className="w-full px-10 py-5 bg-gray-400 rounded-md">
+                        <div className="w-full px-10 py-5 bg-gray-400 rounded-md absolute">
                             {
                                 foodItems.map((item) => {
                                     return (
@@ -58,22 +58,23 @@ const Track = () => {
                         null
                 }
             </div>
-            <div className="w-52 py-8 flex flex-wrap gap-3">
+            <div className="w-52 py-8 flex flex-wrap space-y-2">
                 <div className="w-full h-52 bg-white"></div>
                 <h2 className="font-semibold text-xl">Food Name (100Cal)</h2>
-                <div>
+                <div className="w-1/2 py-3">
                     <p>Protine</p>
                     <p>50</p>
                 </div>
-                <div>
-                    <p>Fat</p>
+                <div className="w-1/2 py-3">
+                    <p>Fats</p>
                     <p>20</p>
                 </div>
-                <div>
-                    <p>Carp</p>
+                <div className="w-1/2 py-3">
+                    <p>Carbs</p>
                     <p>30</p>
                 </div>
-                <input type="number" placeholder="َQuantity in Grams" />
+                <input className="px-2 py-1 rounded-md" type="number" placeholder="َQuantity in Gms" />
+                <button className="px-2 py-1 bg-[#007654] text-xl font-bold text-white rounded-lg">Track This Food</button>
             </div>
         </section>
     )
